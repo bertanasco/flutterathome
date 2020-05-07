ad6054081ddd3adb9eb75e886bd2231a0c4f1bfd
banasco <bertanasco@gmail.com>
2020-05-07 18:30:28 +0800

    in progress blake's card

--- appbrewery/section_06/mi_card_flutter/lib/main.dart
+++ appbrewery/section_06/mi_card_flutter/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.teal,
+          backgroundColor: Colors.teal.shade600,
           body: SafeArea(
             child: Column(
               children: <Widget>[
@@ -19,21 +19,64 @@ class MyApp extends StatelessWidget {
                   radius: 50,
                 ),
                 Text(
-                  'Blake Caden Añasco',
+                  'Caden Añasco',
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 35,
+                    fontFamily: 'Pacifico',
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
                 Text(
-                  'Planter Destroyer',
+                  'PLANET DESTROYER',
                   style: TextStyle(
-                    color: Colors.white,
+                    color: Colors.teal.shade100,
+                    fontFamily: 'SourceSansPro',
                     fontSize: 20,
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 25,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade800,
+                    ),
+                    title: Text(
+                      '+63 917 1433 123',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal.shade800,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 25,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal.shade800,
+                    ),
+                    title: Text(
+                      'blakecaden@mama.com',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal.shade800,
+                      ),
+                    ),
                   ),
                 ),
               ],
-              mainAxisAlignment: MainAxisAlignment.center,
             ),
           )),
     );
