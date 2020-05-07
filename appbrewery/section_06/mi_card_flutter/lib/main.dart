6d6bc0ac56350475ef6f00f67ffa1f498e1ac771
banasco <bertanasco@gmail.com>
2020-05-07 16:18:42 +0800

    added safe area for section 6

--- appbrewery/section_06/mi_card_flutter/lib/main.dart
+++ appbrewery/section_06/mi_card_flutter/lib/main.dart
@@ -9,17 +9,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.teal[800],
-          title: Text('Test'),
-        ),
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Text('Hello'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.red,
-          child: Icon(Icons.add),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            child: Text('test'),
+          ),
         ),
       ),
     );
