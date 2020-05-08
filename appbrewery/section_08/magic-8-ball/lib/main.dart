30389e0cec601016163d98a490aef41466895d2a
banasco <bertanasco@gmail.com>
2020-05-08 12:01:15 +0800

    section 8 step 2

--- appbrewery/section_08/magic-8-ball/lib/main.dart
+++ appbrewery/section_08/magic-8-ball/lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(Magic8Ball());
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade800,
+          title: Text(
+            'Ask Me Anything',
+            textAlign: TextAlign.right,
+          ),
+        ),
+        body: Container(
+          color: Colors.blue,
+        ),
       ),
-    );
+    ));
+  }
+}
