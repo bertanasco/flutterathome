20eac0dafe1c8504c17ed3fea69cda108bd1e2e2
banasco <bertanasco@gmail.com>
2020-05-08 13:57:40 +0800

    Layout challenge

--- appbrewery/section_09/xylophone/lib/main.dart
+++ appbrewery/section_09/xylophone/lib/main.dart
@@ -9,54 +9,84 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$noteNumber.wav');
   }
 
+  List<Widget> generateFlatButtons() {
+    List<Widget> flatButtons = List<Widget>();
+    for (var i = 1; i < 8; i++) {
+      flatButtons.add(
+        FlatButton(
+          color: Colors.red,
+          onPressed: () {
+            playNote(i);
+          },
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  playNote(1);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    playNote(1);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.orange,
-                onPressed: () {
-                  playNote(2);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    playNote(2);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.yellow,
-                onPressed: () {
-                  playNote(3);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    playNote(3);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.green,
-                onPressed: () {
-                  playNote(4);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    playNote(4);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.greenAccent,
-                onPressed: () {
-                  playNote(5);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.greenAccent,
+                  onPressed: () {
+                    playNote(5);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  playNote(6);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    playNote(6);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.deepPurple,
-                onPressed: () {
-                  playNote(7);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.deepPurple,
+                  onPressed: () {
+                    playNote(7);
+                  },
+                ),
               ),
             ],
           ),
