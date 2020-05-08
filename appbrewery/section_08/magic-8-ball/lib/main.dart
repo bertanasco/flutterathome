754840c7fdc357552030a881daaf195af45f687e
banasco <bertanasco@gmail.com>
2020-05-08 12:09:36 +0800

    not that much boss level

--- appbrewery/section_08/magic-8-ball/lib/main.dart
+++ appbrewery/section_08/magic-8-ball/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(Magic8Ball());
@@ -8,6 +10,14 @@ class Magic8Ball extends StatefulWidget {
 }
 
 class _Magic8BallState extends State<Magic8Ball> {
+  int ballNumber = Random().nextInt(5) + 1;
+
+  void randomBallNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return (MaterialApp(
@@ -20,7 +30,15 @@ class _Magic8BallState extends State<Magic8Ball> {
           ),
         ),
         body: Container(
-          color: Colors.blue,
+          color: Colors.white,
+          child: Center(
+            child: FlatButton(
+                child: Image.asset('images/ball$ballNumber.png'),
+                onPressed: () {
+                  randomBallNumber();
+                  print('ball number $ballNumber');
+                }),
+          ),
         ),
       ),
     ));
